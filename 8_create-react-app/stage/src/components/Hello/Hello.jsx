76e4ce90b6ada5e270f51d7de68caea9d198c661
水冗水孚 <lss13611833316@163.com>
2025-05-04 00:25:24 +0800

    快捷键

--- 8_create-react-app/stage/src/components/Hello/Hello.jsx
+++ 8_create-react-app/stage/src/components/Hello/Hello.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+// 快捷键rcc和rfc
+// rcc: class组件 React Class Component
+// rfc: 函数组件 React Function Component
 export default class Hello extends Component {
     render() {
         return (
