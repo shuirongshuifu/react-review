e34d47c7b022399a90568f048047b5e0e8b5ab68
水冗水孚 <lss13611833316@163.com>
2025-05-04 00:22:05 +0800

    复习到脚手架Create React App的使用

---
+++ 8_create-react-app/stage/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './components/Hello/Hello';
+import Welcome from './components/Welcome/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello></Hello>
+      <Welcome></Welcome>
+    </div>
+  );
+}
+
+export default App;
